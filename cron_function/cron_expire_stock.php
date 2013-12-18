dc10cbed26f68c93087ae96c09d010afc9d7d7fb
elmer <e.quilala@spinweb.ph>
2013-12-18 16:48:29 +0800

    Update comments migration

--- cron_function/cron_expire_stock.php
+++ cron_function/cron_expire_stock.php
@@ -1,5 +1,11 @@
 <?php 
+/*
+	A package so you can call function in a module on a php file.
+*/
 include "bootstrap_setup.php";
+/*
+	you can this function under this file:<root>/sites/all/modules/uc_sm_appliance_customization/shipping_accomodation/ shipping_accomodation.module   
+*/
 cron_expire_stock();
 //
 //print xend_to_excel(date("Y-m-d",$daybefore),TRUE,"jyc@mci.com.ph,rad@mci.com.ph,ecd@mci.com.ph"); 
--- cron_function/sku_update_scheduler.php
+++ cron_function/sku_update_scheduler.php
@@ -1,5 +1,11 @@
 <?php 
+/*
+	A package so you can call function in a module on a php file.
+*/
 include "bootstrap_setup.php";
+/*
+	you can this function under this file:<root>/sites/all/modules/uc_sm_appliance_customization/mms_generator/mms_generator.module
+*/
 $daybefore =  strtotime("now");
 print cron_sku_update_check(date("Y-m-d",$daybefore));
 ?>
